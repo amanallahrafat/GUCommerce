0b560473c4223a62b450af9387169b36b3b22ff3
mohamedFabdrabo <42771081+mohamedFabdrabo@users.noreply.github.com>
2019-11-13 15:22:58 +0200

    tables without constrains

--- queries.sql
+++ queries.sql
@@ -70,6 +70,7 @@ FOREIGN KEY(username) REFERENCES Admins
 )
 
 --Datatypes have to be discussed in Orders Table
+
 CREATE TABLE Orders
 (order_no INT PRIMARY KEY,
 order_date datetime,
@@ -82,7 +83,9 @@ FOREIGN KEY (Gift_Card_code_used) REFERENCES Giftcard,
 FOREIGN KEY(customer_name) REFERENCES Customer,
 FOREIGN KEY(delivery_id) REFERENCES Delivery,
 FOREIGN KEY(creditCard_number) REFERENCES Credit_Card
+
 )
+
 
 CREATE TABLE Product
 (
@@ -91,10 +94,133 @@ product_name varchar(20),
 category varchar(20),
 product_description text,
 final_price decimal(10,2),
-color varchar(20), available BIT, rate INT,
-vendor_username varchar(20), customer_username varchar(20),
+color varchar(20),
+available BIT,
+rate INT,
+vendor_username varchar(20),
+customer_username varchar(20),
 customer_order_id INT,
+FOREIGN KEY(vendor_username) REFERENCES Vendor,
 FOREIGN KEY(customer_order_id) REFERENCES Orders,
-FOREIGN KEY(customer_name) REFERENCES Customer,
-FOREIGN KEY(vendor_username) REFERENCES Vendor
+FOREIGN KEY(customer_username) REFERENCES Customer
 )
+
+CREATE TABLE CustomerAddstoCartProduct
+(
+serial_no INT PRIMARY KEY,
+customer_name varchar(20),
+FOREIGN KEY(serial_no) REFERENCES Product,
+FOREIGN KEY(customer_name) REFERENCES Customer
+)
+
+CREATE TABLE Todays_Deals
+(
+deal_id INT PRIMARY KEY,
+deal_amount INT,
+expiry_date datetime,
+admin_username VARCHAR(20),
+FOREIGN KEY(admin_username) REFERENCES Admins
+)
+
+CREATE TABLE Todays_Deals_Product
+(
+deal_id INT,
+serial_no INT, 
+issue_date datetime,
+CONSTRAINT PK_Todays_Deals_Product PRIMARY KEY (deal_id,serial_no),
+FOREIGN KEY(deal_id) REFERENCES Todays_Deals,
+FOREIGN KEY(serial_no) REFERENCES Product
+)
+
+CREATE TABLE offer
+(
+offer_id INT IDENTITY(1,1) PRIMARY KEY,
+offer_amount INT,
+expiry_date DATETIME
+)
+
+CREATE TABLE offersOnProduct
+(
+offer_id INT,
+serial_no INT,
+CONSTRAINT PK PRIMARY KEY (offer_id,serial_no),
+FOREIGN KEY(serial_no) REFERENCES Product,
+FOREIGN KEY(offer_id) REFERENCES offer
+)
+
+CREATE TABLE Customer_Question_Product
+(
+serial_no INT,
+customer_name VARCHAR(20),
+question varchar(50), 
+answer text,
+CONSTRAINT PK_CQP PRIMARY KEY (serial_no,customer_name),
+FOREIGN KEY(serial_no) REFERENCES Product,
+FOREIGN KEY(customer_name) REFERENCES Customer
+)
+CREATE TABLE Wishlist
+(
+username VARCHAR(20),
+name VARCHAR(20),
+CONSTRAINT PK_WL PRIMARY KEY (username,name),
+FOREIGN KEY(username) REFERENCES Customer
+)
+
+CREATE TABLE Giftcard
+( 
+code varchar(10) PRIMARY KEY,
+expiry_date datetime,
+amount INT,
+username varchar(20),
+FOREIGN KEY(username) REFERENCES Admins
+)
+
+CREATE TABLE Wishlist_Product
+(
+username VARCHAR(20),
+wish_name VARCHAR(20),
+serial_no INT,
+FOREIGN KEY(username, wish_name) REFERENCES Wishlist,
+FOREIGN KEY(serial_no) REFERENCES Product
+)
+
+CREATE TABLE Admin_Customer_Giftcard
+(
+code INT ,
+customer_name VARCHAR(20),
+admin_username VARCHAR(20),
+remaining_points INT,
+CONSTRAINT PK_ACG PRIMARY KEY (code,customer_name,admin_username),
+FOREIGN KEY(code) REFERENCES Giftcard,
+FOREIGN KEY(customer_name) REFERENCES Customer,
+FOREIGN KEY(admin_username) REFERENCES Admins
+)
+
+CREATE TABLE Admin_Delivery_Order
+(
+delivery_username VARCHAR(20),
+order_no INT,
+admin_username VARCHAR(20),
+delivery_window VARCHAR(50),
+CONSTRAINT PK_ADO PRIMARY KEY (delivery_username,order_no),
+FOREIGN KEY(delivery_username) REFERENCES Delivery_person,
+FOREIGN KEY(order_no) REFERENCES orders,
+FOREIGN KEY(admin_username) REFERENCES Admins
+)
+
+CREATE TABLE Customer_CreditCard
+(
+customer_name VARCHAR(20),
+cc_number varchar(20) ,
+CONSTRAINT PK_CCC PRIMARY KEY (customer_name,cc_number),
+FOREIGN KEY(customer_name) REFERENCES Customer,
+FOREIGN KEY(cc_number) REFERENCES Credit_Card
+)
+
+
+
+
+
+
+
+
