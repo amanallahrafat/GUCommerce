101af6260b87270f63fccf8636a24dc112482b15
mohamedFabdrabo <42771081+mohamedFabdrabo@users.noreply.github.com>
2019-11-15 15:54:27 +0200

    Update procedures_and_functions.sql

--- procedures_and_functions.sql
+++ procedures_and_functions.sql
@@ -1,10 +1,6 @@
+-- Fathy's procedures
 
-CREATE PROC vendorRegister
-@username varchar(20),@first_name varchar(20), @last_name varchar(20),@password varchar(20),
-@email varchar(50), @company_name varchar(20), @bank_acc_no varchar(20)
-AS
-INSERT INTO Users 
-values (@username,@password, @first_name, @last_name, @email);
-INSERT INTO Vendor (username,company_name,bank_acc_no)
-values (@username, @company_name, @bank_acc_no);
-GO;
+
+
+
+--end of fathy's procedures
