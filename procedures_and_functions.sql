ff313f6730a8a813c73126bfa1148174a9268df4
Mahmoud Reda <mahmoud.reda@example.com>
2019-11-18 22:37:33 +0200

    Update procedures_and_functions.sql

--- procedures_and_functions.sql
+++ procedures_and_functions.sql
@@ -1,11 +1,43 @@
-
-CREATE PROC vendorRegister
-@username varchar(20),@first_name varchar(20), @last_name varchar(20),@password varchar(20),
-@email varchar(50), @company_name varchar(20), @bank_acc_no varchar(20)
+-- Fathy's procedures
+GO
+CREATE PROC customerRegister
+@username varchar(20),
+@first_name varchar(20),
+@last_name varchar(20),
+@password varchar(20),
+@email varchar(50)
 AS
+INSERT INTO Users
+values(@username ,@password, @first_name,@last_name,@email)
+
+INSERT INTO Customer
+values(@username ,0)
+END
+
+GO
+CREATE PROC vendorRegister
+@username varchar(20),
+@first_name varchar(20),
+@last_name varchar(20),
+@password varchar(20),
+@email varchar(50),
+@company_name varchar(20), 
+@bank_acc_no varchar(20)
+AS
+<<<<<<< HEAD
 INSERT INTO Users 
 values (@username,@password, @first_name, @last_name, @email);
 INSERT INTO Vendor (username,company_name,bank_acc_no)
 values (@username, @company_name, @bank_acc_no);
 
 
+=======
+INSERT INTO Users
+values(@username ,@password, @first_name,@last_name,@email)
+
+INSERT INTO Vendor(username,company_name,bank_acc_no)
+values(@username, @company_name , @bank_acc_no)
+END
+
+--end of fathy's procedures
+>>>>>>> 08922fb9b40bd6d670b09e2e2caddcf0d2fc2189
