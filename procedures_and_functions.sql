d955c24e2b9ab8b38ca8084950216d962c4bee85
Mahmoud Reda <mahmoud.reda@example.com>
2019-11-19 00:08:05 +0200

    Update procedures_and_functions.sql

--- procedures_and_functions.sql
+++ procedures_and_functions.sql
@@ -1,5 +1,3 @@
--- Fathy's procedures
-GO
 CREATE PROC customerRegister
 @username varchar(20),
 @first_name varchar(20),
@@ -9,12 +7,11 @@ CREATE PROC customerRegister
 AS
 INSERT INTO Users
 values(@username ,@password, @first_name,@last_name,@email)
-
 INSERT INTO Customer
 values(@username ,0)
-END
+GO;
 
-GO
+
 CREATE PROC vendorRegister
 @username varchar(20),
 @first_name varchar(20),
@@ -24,20 +21,31 @@ CREATE PROC vendorRegister
 @company_name varchar(20), 
 @bank_acc_no varchar(20)
 AS
-<<<<<<< HEAD
 INSERT INTO Users 
 values (@username,@password, @first_name, @last_name, @email);
 INSERT INTO Vendor (username,company_name,bank_acc_no)
 values (@username, @company_name, @bank_acc_no);
+GO;
 
 
-=======
-INSERT INTO Users
-values(@username ,@password, @first_name,@last_name,@email)
-
-INSERT INTO Vendor(username,company_name,bank_acc_no)
-values(@username, @company_name , @bank_acc_no)
-END
-
---end of fathy's procedures
->>>>>>> 08922fb9b40bd6d670b09e2e2caddcf0d2fc2189
+CREATE PROC userlogin
+@username varchar(20),
+@password varchar(20),
+@success bit OUTPUT,
+@type INT OUTPUT
+AS
+IF EXISTS (SELECT * FROM Users WHERE username LIKE @username AND password LIKE @password)
+	BEGIN
+	SET @success = '1'
+	IF(EXISTS (SELECT * FROM Customer WHERE username LIKE @username))
+		SET @type = 0
+	ELSE IF(EXISTS (SELECT * FROM Vendor WHERE username LIKE @username))
+		SET @type = 1
+	ELSE IF(EXISTS (SELECT * FROM Vendor WHERE username LIKE @username))
+		SET @type = 2
+	ELSE 
+		SET @type = 3
+	END;
+ELSE
+	SET @success = '0'
+GO;
