c311a0473a8aba9234db50c183a46ded2af22c21
Mahmoud Reda <mahmoud.reda@example.com>
2019-11-19 11:46:51 +0200

    Update procedures_and_functions.sql

--- procedures_and_functions.sql
+++ procedures_and_functions.sql
@@ -9,7 +9,7 @@ INSERT INTO Users
 values(@username ,@password, @first_name,@last_name,@email)
 INSERT INTO Customer
 values(@username ,0)
-GO;
+GO
 
 
 CREATE PROC vendorRegister
@@ -25,7 +25,7 @@ INSERT INTO Users
 values (@username,@password, @first_name, @last_name, @email);
 INSERT INTO Vendor (username,company_name,bank_acc_no)
 values (@username, @company_name, @bank_acc_no);
-GO;
+GO
 
 
 CREATE PROC userlogin
@@ -41,11 +41,65 @@ IF EXISTS (SELECT * FROM Users WHERE username LIKE @username AND password LIKE @
 		SET @type = 0
 	ELSE IF(EXISTS (SELECT * FROM Vendor WHERE username LIKE @username))
 		SET @type = 1
-	ELSE IF(EXISTS (SELECT * FROM Vendor WHERE username LIKE @username))
+	ELSE IF(EXISTS (SELECT * FROM Admins WHERE username LIKE @username))
 		SET @type = 2
-	ELSE 
+	ELSE IF(EXISTS (SELECT * FROM Delivery_Person WHERE username LIKE @username))
 		SET @type = 3
+	ELSE
+		SET @type = -1
 	END;
 ELSE
 	SET @success = '0'
-GO;
+GO
+
+
+/*EXEC customerRegister 'ahmed.ashraf', 'ahmed','ashraf','pass123','ahmed@yahoo.com'
+
+EXEC vendorRegister 'eslam.mahmod','eslam' ,'mahmod','pass1234','hopa@gmail.com' ,'Market','132132513'
+DECLARE @success BIT 
+DECLARE @type INT
+EXEC userlogin 'ahmed.ashraf' , 'pass',@success OUTPUT,@type OUTPUT
+PRINT @success
+PRINT @type*/
+
+CREATE PROC addMobile
+@username varchar(20),
+@mobile_number varchar(20)
+AS
+INSERT INTO User_mobile_numbers
+values (@mobile_number, @username)
+GO
+
+CREATE PROC showProducts
+AS
+SELECT * FROM Product
+GO
+
+CREATE PROC ShowProductsbyPrice
+AS
+SELECT * FROM Product
+ORDER BY price
+GO
+
+CREATE PROC searchbyname
+@text varchar(20)
+AS
+SELECT * FROM Product
+WHERE product_name LIKE @text
+GO
+
+CREATE PROC addToCart 
+@customername varchar(20),
+@serial INT
+AS
+INSERT INTO CustomerAddstoCartProduct
+values(@serial, @customername)
+GO
+
+CREATE PROC removefromCart 
+@customername varchar(20),
+@serial INT
+AS
+DELETE FROM CustomerAddstoCartProduct
+WHERE serial_no = @serial AND customer_name LIKE @customername
+GO
